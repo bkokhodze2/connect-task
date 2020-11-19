5e4114a42180607a895debc8bc0ecfe9bde7cbe2
bakuri kokhodze <b_kokhodze2@cu.edu.ge[=]'cleargit config --global user.email b>
2020-11-19 20:35:43 +0400

    add show more btn

--- js.js
+++ js.js
@@ -1,26 +1,54 @@
 $(document).ready(function () {
-  $(".dis0").on("click", function () {
-    $(".div-top").toggleClass("show");
-  });
   
 });
 
-
+//tabs
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "flex";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-  
-  
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
 
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+//showmore showless
+function show() {
+  var dots = document.querySelector("#dots");
+  var moreText = document.querySelector("#more");
+  var btnText = document.querySelector("#showbtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function show2() {
+  var dots = document.querySelector(".text_id > p > #dots");
+  var moreText = document.querySelector(".text_id > p > #more");
+  var btnText = document.querySelector(".text_id > #showbtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
